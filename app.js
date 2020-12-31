8932afa3760dfb9e6fae914ee0d4deaae405bc81
Lashan <lashan779@gmail.com>
2020-12-31 07:56:37 +0530

    Finished The code

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 var todo_ = document.getElementById('todo');
 var plus_btn = document.getElementById('plus');
 
-
 plus_btn.addEventListener('click', addDiv)
 
-
-function addDiv(){
+function addDiv(event){
+    event.preventDefault();
     var div = document.createElement('div')
-    div.classList.toggle('btn-width');
+    div.classList.toggle('todo-div');
     div.innerText = todo_.value;
     document.body.appendChild(div)
 
@@ -23,7 +22,21 @@ function addDiv(){
         div.classList.toggle('completed');
     })
 
+    var btn_delete = document.createElement('btn')
+    btn_delete.innerHTML = "X";
+    btn_delete.classList.toggle('delete-btn')
+    div.appendChild(btn_delete)
 
+    btn_delete.addEventListener('click',() => {
+       div.remove();
+    })
 
-    
+    var btn_check = document.createElement('btn')
+    btn_check.innerHTML = "Check";
+    btn_check.classList.toggle('check-btn')
+    div.appendChild(btn_check)
+
+    btn_check.addEventListener('click',() => {
+       div.classList.toggle('checked')
+    })
 }
